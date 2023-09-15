cb2b39cf9c94832570c18d4559568847f4ddb5a6
ThejanaA <anandthejashwini17@gmail.com>
2023-09-15 13:32:14 +0530

    updated login and sign-up validation.

--- Front-End/DAY 3/src/Pages/Login.js
+++ Front-End/DAY 3/src/Pages/Login.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import '../Assets/CSS/Login.css'
+import React, { useState } from 'react';
+import '../Assets/CSS/Login.css';
 import logo from '../Assets/images/ninja-black.png';
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = formData;
+    let isValid = true;
+    const newErrors = {
+      email: '',
+      password: '',
+    };
+
+    // Email validation
+    if (!email.trim()) {
+      isValid = false;
+      newErrors.email = 'Email is required';
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      isValid = false;
+      newErrors.email = 'Invalid email format';
+    }
+
+    // Password validation
+    if (!password.trim()) {
+      isValid = false;
+      newErrors.password = 'Password is required';
+    }
+
+    setErrors(newErrors);
+
+    if (isValid) {
+      // Submit the form or perform other actions here
+      console.log('Form submitted:', formData);
+    }
+  };
+
   return (
     <>
-  
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-40 w-auto"
-            src={logo}
-            alt="spam-shield"
-          />
+          <img className="mx-auto h-40 w-auto" src={logo} alt="spam-shield" />
           <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
 
         <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -29,18 +75,19 @@ function Login() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.email ? 'border-red-500' : ''}`}
                 />
+                {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" id="password" className="block text-sm font-medium leading-6 text-gray-900 ">
+                <label htmlFor="password" id="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
-                
               </div>
               <div className="mt-2">
                 <input
@@ -48,9 +95,11 @@ function Login() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.password ? 'border-red-500' : ''}`}
                 />
+                {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
               </div>
             </div>
 
@@ -73,6 +122,7 @@ function Login() {
         </div>
       </div>
     </>
-  )
+  );
 }
+
 export default Login;
--- Front-End/DAY 3/src/Pages/Sign-up.js
+++ Front-End/DAY 3/src/Pages/Sign-up.js
@@ -1,25 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Assets/CSS/Login.css';
-
 import logo from '../Assets/images/ninja-black.png';
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password } = formData;
+    let isValid = true;
+    const newErrors = {
+      name: '',
+      email: '',
+      password: '',
+    };
+
+    // Name validation
+    if (!name.trim()) {
+      isValid = false;
+      newErrors.name = 'Full Name is required';
+    }
+
+    // Email validation
+    if (!email.trim()) {
+      isValid = false;
+      newErrors.email = 'Email is required';
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      isValid = false;
+      newErrors.email = 'Invalid email format';
+    }
+
+    // Password validation
+    if (!password.trim()) {
+      isValid = false;
+      newErrors.password = 'Password is required';
+    }
+
+    setErrors(newErrors);
+
+    if (isValid) {
+      // Submit the form or perform other actions here
+      console.log('Form submitted:', formData);
+    }
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-40 w-auto"
-            src={logo}
-            alt="spam-shield"
-          />
+          <img className="mx-auto h-40 w-auto" src={logo} alt="spam-shield" />
           <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign up for an account
           </h2>
         </div>
 
         <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
                 Full Name
@@ -30,9 +84,11 @@ function SignUp() {
                   name="name"
                   type="text"
                   autoComplete="name"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.name ? 'border-red-500' : ''}`}
                 />
+                {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
               </div>
             </div>
 
@@ -46,9 +102,11 @@ function SignUp() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.email ? 'border-red-500' : ''}`}
                 />
+                {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
               </div>
             </div>
 
@@ -62,9 +120,11 @@ function SignUp() {
                   name="password"
                   type="password"
                   autoComplete="new-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.password ? 'border-red-500' : ''}`}
                 />
+                {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
               </div>
             </div>
 
